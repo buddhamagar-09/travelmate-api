df4681fc61754b188cb3f42460f1074385b26047
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-07-24 13:22:29 +0545

    public routes admin routes  separated and sacntum used for authentication

---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PackageController;
+
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+
+// Authentication
+Route::post('/login', [AuthController::class, 'login']);
+
+// Public package routes
+Route::get('/packages', [PackageController::class, 'index']);
+Route::get('/packages/{package}', [PackageController::class, 'show']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Logged-in user
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+
+    // Package Management
+    Route::post('/packages', [PackageController::class, 'store']);
+    Route::put('/packages/{package}', [PackageController::class, 'update']);
+    Route::delete('/packages/{package}', [PackageController::class, 'destroy']);
+});
