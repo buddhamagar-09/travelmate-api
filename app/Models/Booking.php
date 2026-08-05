8d0f0109b2fa8d33a318e80cf4e5d5cfafd9314b
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-08-05 23:31:27 +0545

    booking model created

---
+++ app/Models/Booking.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    protected $fillable = [
+
+        'user_id',
+        'package_id',
+        'phone_number',
+        'travel_date',
+        'travelers',
+        'special_request',
+        'total_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function package()
+    {
+        return $this->belongsTo(Package::class);
+    }
+}
