b211eff1ea013aec562e3af492079226a19ba066
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-08-10 21:00:00 +0545

    booking table created

---
+++ database/migrations/2026_08_05_155220_create_bookings_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->foreignId('package_id')->constrained()->cascadeOnDelete();
+
+            $table->string('phone_number');
+
+            $table->date('travel_date');
+
+            $table->integer('travelers');
+
+            $table->text('special_request')->nullable();
+
+            $table->decimal('total_price', 10, 2);
+
+            $table->enum('status', [
+                'pending',
+                'confirmed',
+                'cancelled'
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+        
+    }
+};
