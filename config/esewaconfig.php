6b672057671167819e4feadaa2bc94cd086999ab
Buddha Saru Magar <busa_bca2080@lict.edu.np>
2026-08-31 23:43:21 +0545

    Configuration for esewa code,url created

---
+++ config/esewaconfig.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'base_url' => env('ESEWA_BASE_URL'),
+
+    'payment_url' => env('ESEWA_PAYMENT_URL'),
+
+    'product_code' => env('ESEWA_PRODUCT_CODE'),
+
+    'secret_key' => env('ESEWA_SECRET_KEY'),
+
+    'status_url' => env('ESEWA_STATUS_URL'),
+
+];
